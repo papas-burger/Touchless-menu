7fe2d2aad875e064cdb2a68ccb21d96bbcfc9050
Mahmoud Atef <mahmoud.atef98@outlook.com>
2020-12-17 14:39:11 +0200

    Second commit with footer section done

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-for (var i = 0; i < 3; i++) {
-  $(".logo-image").fadeOut(1500).fadeIn(1500);
+// for (var i = 0;i < 3; i++) {
+//   $(".logo-image").fadeOut(1500).fadeIn(1500);
+// }
+
+function openInstagramPage() {
+  window.location = "https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Fpapa_burger_sc%3Figshid%3D3uqsulga8dgg%26fbclid%3DIwAR2VamIhE3Ykrx9IO5fjBwH1i38LQR6ymlq6tb0CWiEMzOS_YAHPBlUU4ic&h=AT2sEpNsNKXdwcm6MIEyrh2-LQVrCHdMynjJ4Iqkel5SmZYZJCG2ZtKLQCxhbr77CPkHFwzccPWgceq4Aph7G-WwwormUWQYWBdswyyUZwmJVprSWC3BS99OHbB6sBveW5h2lQ"
+}
+
+function openFacebookPage() {
+  window.location = "https://www.facebook.com/papa.burger.sc"
 }
